200b6b946a95668775cca981ced3adfc225b999f
kaizer-nurik <kerimovnm@mail.ru>
2020-08-14 21:21:52 +0300

    123

--- excel/vvod excel.py
+++ excel/vvod excel.py
@@ -1,5 +1,22 @@
 import openpyxl
 
+#----------------------------------------------------------
+class Program(object):   
+    def __init__(self, arg):
+        self.num = num
+        self.disp = disp 
+        self.spes = spes #особенности
+        self.time = time
+        self.time_sdo = time_sdo
+        self.days = days
+        self.group = group
+        self.people = people
+        self.people_vn = people_vn
+        self.teach = teach
+        self.aud = aud
+        self.skelet = skelet
+        
+
 def pars_lessons(ind,file_1):
     return list(map(lambda s: s.value, file_1['I'+str(ind):'DK'+str(ind)][0][::2]))
 
@@ -14,7 +31,7 @@ def pars_disp(ind,file_1):
     return (ind, disps)
 
 
-
+#----------------------------------------------------------
 file_1 = openpyxl.load_workbook('Приложение №1.xlsx')['ДПО']
 
 pers = {}
@@ -26,6 +43,23 @@ index = 6
 while (file_1['E'+str(index)].value != None):
     name = file_1['E'+str(index)].value
     index, pers[name] = pars_disp(index, file_1)
-    print(index)
 
 
+del file_1
+
+#----------------------------------------------------------
+file_2 = openpyxl.load_workbook('Приложение №2.xlsx')
+#---------------
+file_auds = file_auds['параметры аудиторий']
+
+while (file_auds['A'+str(index)].value != None):
+    number = file_auds['A'+str(index)].value
+    space = file_auds['B'+str(index)].value
+
+#--------
+file_programs = file_2['параметры программ']
+index = 2
+
+while (file_programs['A'+str(index)].value != None):
+    name = file_programs['C'+str(index)].value
+    
