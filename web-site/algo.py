3b412b24ebe730863e1ef711b10ac30cc2722640
NOTfedos <mistkap@gmail.com>
2020-08-15 12:02:37 +0300

    лулщ

--- web-site/algo.py
+++ web-site/algo.py
@@ -1,2 +1,16 @@
+import table_parser
+from os import path
+import json
+
+
+"""      data.json      """
+
+
 def proc():
-    pass
+    data = json.load(open(path.join("excel", "data.json")))
+    # data["plan"] - словарь массивов (приложение 1)
+    # data["cabs"] - массив классов аудиторий (приложение 2)
+    # data["progs"] - массив классов программ (приложение 2)
+    # data["teacher"] - массив классов учителей (приложение 2)
+
+
