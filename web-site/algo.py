cb28bb3748d9d9980890a1818fbf5850aae0107e
NOTfedos <mistkap@gmail.com>
2020-08-15 13:07:01 +0300

    лулщ

--- web-site/algo.py
+++ web-site/algo.py
@@ -1,4 +1,5 @@
 import table_parser
+from datetime import date, timedelta
 from os import path
 import json
 
@@ -8,9 +9,25 @@ import json
 
 def proc():
     data = json.load(open(path.join("excel", "data.json")))
+    current_day = date(2019, 12, 30)
+
+    ans = []
+
     # data["plan"] - словарь массивов (приложение 1)
     # data["cabs"] - массив классов аудиторий (приложение 2)
     # data["progs"] - массив классов программ (приложение 2)
     # data["teacher"] - массив классов учителей (приложение 2)
 
+    for prog in data["progs"]:
+
+
+    while current_day.year < 2021:
+        teach_list = []
+        for group in grops
+        for teacher in data["teacher"]:
+            if teacher.is_free(current_day):
+                teach_list.append(teacher)
+        if current_day.weekday() == 6:
+
+        current_day += timedelta(days=1)
 
