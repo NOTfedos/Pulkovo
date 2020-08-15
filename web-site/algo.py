d13dd317c948aa75471b0dd2041299fbab3ccbb1
NOTfedos <mistkap@gmail.com>
2020-08-15 13:25:55 +0300

    лулщ

--- web-site/algo.py
+++ web-site/algo.py
@@ -7,23 +7,32 @@ import json
 """      data.json      """
 
 
+class Lesson:
+    def __init__(self):
+
+
+
 def proc():
     data = json.load(open(path.join("excel", "data.json")))
     current_day = date(2019, 12, 30)
 
-    ans = []
-
+    group_sch = dict()
+    teacher_sch = dict()
     # data["plan"] - словарь массивов (приложение 1)
     # data["cabs"] - массив классов аудиторий (приложение 2)
     # data["progs"] - массив классов программ (приложение 2)
     # data["teacher"] - массив классов учителей (приложение 2)
 
-    for prog in data["progs"]:
+    for prog in data["progs"]: # формируем список групп
+        for i in range(int(prog.group)):
+            group_sch.update({f"{prog.name}'-'{i+1}": []})
 
+    for teacher in data["teacher"]:
+        teacher_sch.update({teacher.name: []})
 
     while current_day.year < 2021:
         teach_list = []
-        for group in grops
+
         for teacher in data["teacher"]:
             if teacher.is_free(current_day):
                 teach_list.append(teacher)
